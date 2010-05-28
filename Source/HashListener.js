732acc46ec5f8d82949901dcc5f5e3c19f6f8c44
Roy Shea <royshea@gmail.com>
2010-05-27 20:42:41 -0600

    Source: Unifide spacing used with : and {

--- Source/HashListener.js
+++ Source/HashListener.js
@@ -18,18 +18,18 @@ provides: [HashListener]
 */
 
 var HashListener = new Class({
-	Implements : [Options,Events],
-	options : {
-		blank_page : 'blank.html',
-		start : false
+	Implements: [Options,Events],
+	options: {
+		blank_page: 'blank.html',
+		start: false
 	},
-	iframe : null,
-	currentHash : '',
-	firstLoad : true,
-	handle : false,
-	useIframe : (Browser.Engine.trident && (typeof(document.documentMode)=='undefined' || document.documentMode < 8)),
-	ignoreLocationChange : false,
-	initialize : function(options){
+	iframe: null,
+	currentHash: '',
+	firstLoad: true,
+	handle: false,
+	useIframe: (Browser.Engine.trident && (typeof(document.documentMode)=='undefined' || document.documentMode < 8)),
+	ignoreLocationChange: false,
+	initialize: function (options) {
 		var self=this;
 
 		this.setOptions(options);
@@ -44,7 +44,7 @@ var HashListener = new Class({
         if (
 			window.onhashchange &&
             (typeof(document.documentMode) == 'undefined' || document.documentMode > 7)
-		   ){
+		   ) {
 				// The HTML5 way of handling DHTML history...
 				window.onhashchange = function () {
 					var hash = self.getHash();
@@ -54,18 +54,18 @@ var HashListener = new Class({
 					self.fireEvent('hash-changed',hash);
 				};
         } else  {
-			if (this.useIframe){
+			if (this.useIframe) {
 				this.initializeHistoryIframe();
 			}
         }
 
-		window.addEvent('unload', function(event) {
+		window.addEvent('unload', function (event) {
 			self.firstLoad = null;
 		});
 
 		if (this.options.start) this.start();
 	},
-	initializeHistoryIframe : function(){
+	initializeHistoryIframe: function () {
 		var hash = this.getHash(), doc;
 		this.iframe = new IFrame({
 			src		: this.options.blank_page,
@@ -79,24 +79,24 @@ var HashListener = new Class({
 			}
 		}).inject(document.body);
 
-		doc	= (this.iframe.contentDocument) ? this.iframe.contentDocument  : this.iframe.contentWindow.document;
+		doc	= (this.iframe.contentDocument) ? this.iframe.contentDocument : this.iframe.contentWindow.document;
 		doc.open();
 		doc.write('<html><body id="state">' + hash + '</body></html>');
 		doc.close();
 		return;
 	},
-	checkHash : function(){
+	checkHash: function () {
 		var hash = this.getHash(), ie_state, doc;
 		if (this.ignoreLocationChange) {
 			this.ignoreLocationChange = false;
 			return;
 		}
 
-		if (this.useIframe){
-			doc	= (this.iframe.contentDocument) ? this.iframe.contentDocumnet  : this.iframe.contentWindow.document;
+		if (this.useIframe) {
+			doc	= (this.iframe.contentDocument) ? this.iframe.contentDocumnet : this.iframe.contentWindow.document;
 			ie_state = doc.body.innerHTML;
 
-			if (ie_state!=hash){
+			if (ie_state!=hash) {
 				this.setHash(ie_state);
 				hash = ie_state;
 			}
@@ -110,30 +110,30 @@ var HashListener = new Class({
 
 		this.fireEvent('hash-changed',hash);
 	},
-	setHash : function(newHash){
+	setHash: function (newHash) {
 		window.location.hash = this.currentLocation = newHash;
 
 		this.fireEvent('hash-changed',newHash);
 	},
-	getHash : function(){
+	getHash: function () {
 		var m;
-		if (Browser.Engine.gecko){
+		if (Browser.Engine.gecko) {
 			m = /#(.*)$/.exec(window.location.href);
 			return m && m[1] ? m[1] : '';
-		}else if (Browser.Engine.webkit){
+		}else if (Browser.Engine.webkit) {
 			return decodeURI(window.location.hash.substr(1));
 		}else{
 			return window.location.hash.substr(1);
 		}
 	},
-	setIframeHash: function(newHash) {
-		var doc	= (this.iframe.contentDocument) ? this.iframe.contentDocumnet  : this.iframe.contentWindow.document;
+	setIframeHash: function (newHash) {
+		var doc	= (this.iframe.contentDocument) ? this.iframe.contentDocumnet : this.iframe.contentWindow.document;
 		doc.open();
 		doc.write('<html><body id="state">' + newHash + '</body></html>');
 		doc.close();
 
 	},
-	updateHash : function (newHash){
+	updateHash: function (newHash) {
 		if ($type(document.id(newHash))) {
 			this.debug_msg(
 				"Exception: History locations can not have the same value as _any_ IDs that might be in the document,"
@@ -148,10 +148,10 @@ var HashListener = new Class({
 		if (this.useIframe) this.setIframeHash(newHash);
 		else this.setHash(newHash);
 	},
-	start : function(){
+	start: function () {
 		this.handle = this.checkHash.periodical(100, this);
 	},
-	stop : function(){
+	stop: function () {
 		$clear(this.handle);
 	}
 });
