0557ecb749f71f429edc0eea4aea4b0bdecce57f
Roy Shea <royshea@gmail.com>
2010-05-27 20:47:04 -0600

    Source: Replace tabs with spaces

--- Source/HashListener.js
+++ Source/HashListener.js
@@ -18,140 +18,140 @@ provides: [HashListener]
 */
 
 var HashListener = new Class({
-	Implements: [Options,Events],
-	options: {
-		blank_page: 'blank.html',
-		start: false
-	},
-	iframe: null,
-	currentHash: '',
-	firstLoad: true,
-	handle: false,
-	useIframe: (Browser.Engine.trident && (typeof(document.documentMode)=='undefined' || document.documentMode < 8)),
-	ignoreLocationChange: false,
-	initialize: function (options) {
-		var self=this;
+    Implements: [Options,Events],
+    options: {
+        blank_page: 'blank.html',
+        start: false
+    },
+    iframe: null,
+    currentHash: '',
+    firstLoad: true,
+    handle: false,
+    useIframe: (Browser.Engine.trident && (typeof(document.documentMode)=='undefined' || document.documentMode < 8)),
+    ignoreLocationChange: false,
+    initialize: function (options) {
+        var self=this;
 
-		this.setOptions(options);
+        this.setOptions(options);
 
-		// Disable Opera's fast back/forward navigation mode
-		if (Browser.Engine.presto && window.history.navigationMode) {
-			window.history.navigationMode = 'compatible';
-		}
-
-
-		 // IE8 in IE7 mode defines window.onhashchange, but never fires it...
-        if (
-			window.onhashchange &&
-            (typeof(document.documentMode) == 'undefined' || document.documentMode > 7)
-		   ) {
-				// The HTML5 way of handling DHTML history...
-				window.onhashchange = function () {
-					var hash = self.getHash();
-					if (hash == self.currentHash) {
-						return;
-					}
-					self.fireEvent('hash-changed',hash);
-				};
-        } else  {
-			if (this.useIframe) {
-				this.initializeHistoryIframe();
-			}
+        // Disable Opera's fast back/forward navigation mode
+        if (Browser.Engine.presto && window.history.navigationMode) {
+            window.history.navigationMode = 'compatible';
         }
 
-		window.addEvent('unload', function (event) {
-			self.firstLoad = null;
-		});
 
-		if (this.options.start) this.start();
-	},
-	initializeHistoryIframe: function () {
-		var hash = this.getHash(), doc;
-		this.iframe = new IFrame({
-			src: this.options.blank_page,
-			styles: {
-				'position': 'absolute',
-				'top': 0,
-				'left': 0,
-				'width': '1px',
-				'height': '1px',
-				'visibility': 'hidden'
-			}
-		}).inject(document.body);
+         // IE8 in IE7 mode defines window.onhashchange, but never fires it...
+        if (
+            window.onhashchange &&
+            (typeof(document.documentMode) == 'undefined' || document.documentMode > 7)
+           ) {
+                // The HTML5 way of handling DHTML history...
+                window.onhashchange = function () {
+                    var hash = self.getHash();
+                    if (hash == self.currentHash) {
+                        return;
+                    }
+                    self.fireEvent('hash-changed',hash);
+                };
+        } else  {
+            if (this.useIframe) {
+                this.initializeHistoryIframe();
+            }
+        }
 
-		doc = (this.iframe.contentDocument) ? this.iframe.contentDocument : this.iframe.contentWindow.document;
-		doc.open();
-		doc.write('<html><body id="state">' + hash + '</body></html>');
-		doc.close();
-		return;
-	},
-	checkHash: function () {
-		var hash = this.getHash(), ie_state, doc;
-		if (this.ignoreLocationChange) {
-			this.ignoreLocationChange = false;
-			return;
-		}
+        window.addEvent('unload', function (event) {
+            self.firstLoad = null;
+        });
 
-		if (this.useIframe) {
-			doc = (this.iframe.contentDocument) ? this.iframe.contentDocumnet : this.iframe.contentWindow.document;
-			ie_state = doc.body.innerHTML;
+        if (this.options.start) this.start();
+    },
+    initializeHistoryIframe: function () {
+        var hash = this.getHash(), doc;
+        this.iframe = new IFrame({
+            src: this.options.blank_page,
+            styles: {
+                'position': 'absolute',
+                'top': 0,
+                'left': 0,
+                'width': '1px',
+                'height': '1px',
+                'visibility': 'hidden'
+            }
+        }).inject(document.body);
 
-			if (ie_state!=hash) {
-				this.setHash(ie_state);
-				hash = ie_state;
-			}
-		}
+        doc = (this.iframe.contentDocument) ? this.iframe.contentDocument : this.iframe.contentWindow.document;
+        doc.open();
+        doc.write('<html><body id="state">' + hash + '</body></html>');
+        doc.close();
+        return;
+    },
+    checkHash: function () {
+        var hash = this.getHash(), ie_state, doc;
+        if (this.ignoreLocationChange) {
+            this.ignoreLocationChange = false;
+            return;
+        }
 
-		if (this.currentLocation == hash) {
-			return;
-		}
+        if (this.useIframe) {
+            doc = (this.iframe.contentDocument) ? this.iframe.contentDocumnet : this.iframe.contentWindow.document;
+            ie_state = doc.body.innerHTML;
 
-		this.currentLocation = hash;
+            if (ie_state!=hash) {
+                this.setHash(ie_state);
+                hash = ie_state;
+            }
+        }
 
-		this.fireEvent('hash-changed',hash);
-	},
-	setHash: function (newHash) {
-		window.location.hash = this.currentLocation = newHash;
+        if (this.currentLocation == hash) {
+            return;
+        }
 
-		this.fireEvent('hash-changed',newHash);
-	},
-	getHash: function () {
-		var m;
-		if (Browser.Engine.gecko) {
-			m = /#(.*)$/.exec(window.location.href);
-			return m && m[1] ? m[1] : '';
-		}else if (Browser.Engine.webkit) {
-			return decodeURI(window.location.hash.substr(1));
-		}else{
-			return window.location.hash.substr(1);
-		}
-	},
-	setIframeHash: function (newHash) {
-		var doc = (this.iframe.contentDocument) ? this.iframe.contentDocumnet : this.iframe.contentWindow.document;
-		doc.open();
-		doc.write('<html><body id="state">' + newHash + '</body></html>');
-		doc.close();
+        this.currentLocation = hash;
 
-	},
-	updateHash: function (newHash) {
-		if ($type(document.id(newHash))) {
-			this.debug_msg(
-				"Exception: History locations can not have the same value as _any_ IDs that might be in the document,"
-				+ " due to a bug in IE; please ask the developer to choose a history location that does not match any HTML"
-				+ " IDs in this document. The following ID is already taken and cannot be a location: "
-				+ newLocation
-			);
-		}
+        this.fireEvent('hash-changed',hash);
+    },
+    setHash: function (newHash) {
+        window.location.hash = this.currentLocation = newHash;
 
-		this.ignoreLocationChange = true;
+        this.fireEvent('hash-changed',newHash);
+    },
+    getHash: function () {
+        var m;
+        if (Browser.Engine.gecko) {
+            m = /#(.*)$/.exec(window.location.href);
+            return m && m[1] ? m[1] : '';
+        }else if (Browser.Engine.webkit) {
+            return decodeURI(window.location.hash.substr(1));
+        }else{
+            return window.location.hash.substr(1);
+        }
+    },
+    setIframeHash: function (newHash) {
+        var doc = (this.iframe.contentDocument) ? this.iframe.contentDocumnet : this.iframe.contentWindow.document;
+        doc.open();
+        doc.write('<html><body id="state">' + newHash + '</body></html>');
+        doc.close();
 
-		if (this.useIframe) this.setIframeHash(newHash);
-		else this.setHash(newHash);
-	},
-	start: function () {
-		this.handle = this.checkHash.periodical(100, this);
-	},
-	stop: function () {
-		$clear(this.handle);
-	}
+    },
+    updateHash: function (newHash) {
+        if ($type(document.id(newHash))) {
+            this.debug_msg(
+                "Exception: History locations can not have the same value as _any_ IDs that might be in the document,"
+                + " due to a bug in IE; please ask the developer to choose a history location that does not match any HTML"
+                + " IDs in this document. The following ID is already taken and cannot be a location: "
+                + newLocation
+            );
+        }
+
+        this.ignoreLocationChange = true;
+
+        if (this.useIframe) this.setIframeHash(newHash);
+        else this.setHash(newHash);
+    },
+    start: function () {
+        this.handle = this.checkHash.periodical(100, this);
+    },
+    stop: function () {
+        $clear(this.handle);
+    }
 });
